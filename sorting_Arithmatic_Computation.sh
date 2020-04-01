2c9d9c3904955b3af0ec614573cd2a6b6125382c
pranjalikawale <pranjalikawale1578@gmail.com>
2020-03-31 13:02:20 -1200

    ADDED: Compute a%b+c

--- sorting_Arithmatic_Computation.sh
+++ sorting_Arithmatic_Computation.sh
@@ -2,5 +2,5 @@
 
 read -p "Enter the three input: " a b c
 
-echo "Result of ( $c + $a / $b ) is" 
-awk "BEGIN {print $c+$a/$b }"
+echo "Result of ( $a % $b + $c ) is" 
+awk "BEGIN {print $a%$b+$c }"
