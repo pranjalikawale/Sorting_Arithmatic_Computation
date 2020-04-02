03d651043ca9a5ee2b5d3013ccf4a9db6cba63d3
pranjalikawale <pranjalikawale1578@gmail.com>
2020-04-01 15:16:47 -1200

    ADDED: Dictionary store result

--- sortingArithmaticComputation.sh
+++ sortingArithmaticComputation.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -x
 
 read -p "Enter the three input: " a b c
-
 declare -A result
 
 result[" $a+$b*$c "]=$((a+b*c))
@@ -10,5 +9,5 @@ result[" $c+$a/$b "]=`awk "BEGIN {print $c+$a/$b }"`
 result[" $a%$b+$c "]=`awk "BEGIN {print $a%$b+$c }"`
 
 echo "Expression "${!result[@]}
-echo "Result " ${result[@]}
+echo "Result "${result[@]}
 
