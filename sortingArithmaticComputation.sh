2c69309a2e66d3d01da72e70e58dc7bce2083958
pranjalikawale <pranjalikawale1578@gmail.com>
2020-04-01 14:20:44 -1200

    MODIFIED: Fixed bug of comparing float value

--- sortingArithmaticComputation.sh
+++ sortingArithmaticComputation.sh
@@ -6,19 +6,17 @@ declare -A result
 
 result[" $a+$b*$c "]=$((a+b*c))
 result[" $a*$b+$c "]=$((a*b+c))
-result[" $c+$a/$b "]=$((c+a/b))
+result[" $c+$a/$b "]=`awk "BEGIN {print $c+$a/$b }"`
 result[" $a%$b+$c "]=`awk "BEGIN {print $a%$b+$c }"`
 
 
 function create_Array()
 {
 	count=0
-
 	for a in "${@}"
 	do
 		resArray[((count++))]=$a
 	done	
-
 	sorting "${resArray[@]}"
 }
 
@@ -30,7 +28,7 @@ function sorting()
 	do
 		for ((j=$((i+1));j<${#arr[@]};j++))
 		do
-			if [[ ${arr[$i]} -lt ${arr[$j]} ]]
+			if [[ ${arr[$i]} < ${arr[$j]} ]]
 			then
 				temp=${arr[$i]}
 				arr[$i]=${arr[$j]}
