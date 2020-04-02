27692fbafb04921a6a539ed32a19117b7e5900a7
pranjalikawale <pranjalikawale1578@gmail.com>
2020-04-01 21:22:59 -1200

    MODIFIED: Insert decending order logic

--- sortingArithmaticComputation.sh
+++ sortingArithmaticComputation.sh
@@ -25,14 +25,28 @@ function sorting()
 	do
 		for ((j=$((i+1));j<${#arr[@]};j++))
 		do
-			if [[ ${arr[$i]} > ${arr[$j]} ]]
-			then
-				temp=${arr[$i]}
-				arr[$i]=${arr[$j]}
-				arr[$j]=$temp
-			fi
+			if [[ ${arr[$i]} < ${arr[$j]} ]]
+         then
+            temp=${arr[$i]}
+            arr[$i]=${arr[$j]}
+            arr[$j]=$temp
+         fi 
 		done
 	done
+	echo "Sort the reasult in decending order: " ${arr[@]}
+	
+	for ((i=0;i<$((${#arr[@]}-1));i++))
+   do
+      for ((j=$((i+1));j<${#arr[@]};j++))
+      do
+         if [[ ${arr[$i]} > ${arr[$j]} ]]
+         then
+            temp=${arr[$i]}
+            arr[$i]=${arr[$j]}
+            arr[$j]=$temp
+         fi
+      done
+   done
 	echo "Sort the reasult in ascending order: " ${arr[@]}
 }
 createArray "${result[@]}"
