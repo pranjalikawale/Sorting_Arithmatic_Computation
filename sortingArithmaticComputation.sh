41c3641a0996e98b3ecd7e6d005efac0d2b76263
pranjalikawale <pranjalikawale1578@gmail.com>
2020-04-02 12:05:53 -1200

    REFACTORED: Variable result rename resultExpression

--- sortingArithmaticComputation.sh
+++ sortingArithmaticComputation.sh
@@ -2,63 +2,63 @@
 
 function addMultiply()
 {
-   echo $(($1+$2*$3))
+	echo $(($1+$2*$3))
 }
 
 function multiplyAdd()
 {
-   echo $(($1*$2+$3))
+	echo $(($1*$2+$3))
 }
 
 function addDivide()
 {
-   echo `awk "BEGIN {print $3+$1/$2 }"`
+	echo `awk "BEGIN {print $3+$1/$2 }"`
 }
 
 function modAdd()
 {
-   echo `awk "BEGIN {print $1%$2+$3 }"`
+	echo `awk "BEGIN {print $1%$2+$3 }"`
 }
 
 function evaluateOperation()
 {
-   declare -A result
-   result[" $a+$b*$c "]=$(addMultiply $a $b $c)
-   result[" $a*$b+$c "]=$(multiplyAdd $a $b $c)
-   result[" $c+$a/$b "]=$(addDivide $a $b $c)
-   result[" $a%$b+$c "]=$(modAdd $a $b $c)
-   createArray "${result[@]}"
+	declare -A resultExpression
+	resultExpression[" $a+$b*$c "]=$(addMultiply $a $b $c)
+	resultExpression[" $a*$b+$c "]=$(multiplyAdd $a $b $c)
+	resultExpression[" $c+$a/$b "]=$(addDivide $a $b $c)
+   resultExpression[" $a%$b+$c "]=$(modAdd $a $b $c)
+   createArray "${resultExpression[@]}"
 }
 
 function createArray()
 {
-   count=0
-   for val in "${@}"
-   do
-      resultArray[((count++))]=$val
-   done
-   sorting "${resultArray[@]}"
+	count=0
+	for val in "${@}"
+	do
+		resultArray[((count++))]=$val
+	done
+	sorting "${resultArray[@]}"
 }
 
 function sorting()
 {
-   array=("${@}")
-   for ((i=0;i<$((${#array[@]}-1));i++))
-   do
-      for ((j=$((i+1));j<${#array[@]};j++))
-      do
-         if [[ ${array[$i]} < ${array[$j]} ]]
-         then
-            temp=${array[$i]}
-            array[$i]=${array[$j]}
-            array[$j]=$temp
-         fi
-      done
-         ascending[${#array[@]}-$i]=${array[$i]}
-   done
-         ascending[${#array[@]}-$i]=${array[$i]}
-   echo "Sort the reasult in decending order: " ${array[@]}
-   echo "Sort the reasult in ascending order: " ${ascending[@]}
+	array=("${@}")
+	for ((i=0;i<$((${#array[@]}-1));i++))
+	do
+		for ((j=$((i+1));j<${#array[@]};j++))
+		do
+			if [[ ${array[$i]} < ${array[$j]} ]]
+			then
+				temp=${array[$i]}
+				array[$i]=${array[$j]}
+				array[$j]=$temp
+			fi
+		done
+		ascending[${#array[@]}-$i]=${array[$i]}
+	done
+	ascending[${#array[@]}-$i]=${array[$i]}
+	echo "Sort the reasult in decending order: " ${array[@]}
+	echo "Sort the reasult in ascending order: " ${ascending[@]}
 }
 
 read -p "Enter the three input: " a b c
